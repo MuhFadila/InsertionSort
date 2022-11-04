62308db3dcfd2e6cde533244e532a28aefc59a71
Muhammad Fadila <muh.fadila18@gmail.com>
2022-11-04 16:23:53 +0700

    add console write line and fungtion int

--- Program.cs
+++ Program.cs
@@ -9,7 +9,12 @@ namespace InsertionSort
     {
         static public void InsertionSorting()
         {
-            
+            Console.Write("\nProgram for sorting a numeric array using Insertion Sorting");
+            Console.Write("\n\nEnter number of elements: ");
+            int max = Convert.ToInt32(Console.ReadLine());
+            int[] numarray = new int[30];
+            for (int i = 0; i < max; i++)
+            { }
         }
     }
 }
