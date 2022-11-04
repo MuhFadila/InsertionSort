c809c6149350cdfef4a83a21b24451f5f9164871
Muhammad Fadila <muh.fadila18@gmail.com>
2022-11-04 16:21:31 +0700

    create public void

--- Program.cs
+++ Program.cs
@@ -3,5 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace InsertionSort { }
+namespace InsertionSort
+{
+    class Program
+    {
+        static public void InsertionSorting()
+        {
+            
+        }
+    }
+}
 
